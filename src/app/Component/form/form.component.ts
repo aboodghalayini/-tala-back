be19f36fa0c621523892f15729b7d6ae22faa24d
Abood Ghalayini <abood.ghalayini@gmail.com>
2023-02-19 22:21:23 +0200

    add nave bare and form

--- src/app/Component/form/form.component.ts
+++ src/app/Component/form/form.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent {
+onSubmit(): void {
+
+
+  
+}
 
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactUsComponent } from './Component/contact-us/contact-us.component';
+import { FormComponent } from './Component/form/form.component';
+import { HomeComponent } from './Component/home/home.component';
+import { LocationComponent } from './Component/location/location.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'location', component: LocationComponent},
+  { path: 'Register', component: FormComponent},
+  { path: 'contact-us', component: ContactUsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
